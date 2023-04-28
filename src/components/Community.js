c8e2886e4371a185e0b9e7db952254199e7194c1
Kartik Patel <kartikpatel0170@gmail.com>
2023-04-28 01:04:01 -0600

    fixed UI, modified the data and added some logos

--- src/components/Community.js
+++ src/components/Community.js
@@ -11,23 +11,23 @@ function CompanyList() {
               <h2 className="text-3xl font-bold mb-6 text-indigo-600">{category.charAt(0).toUpperCase() + category.slice(1)}</h2>
               <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
                 {Object.entries(data[category][0]).map(([key, value]) => (
-                  <li key={key} className="p-8 bg-white border border-gray-300 rounded-lg shadow-md hover:shadow-xl transition duration-300 ease-in-out overflow-hidden">
-                    <div className="flex flex-col sm:flex-col items-center h-full justify-center">
+                  <li key={key} className={`p-8 bg-white border-${Object.keys(data[category][0]).length > 1 ? '2' : '4'} border-gray-300 rounded-lg shadow-md hover:shadow-xl transition duration-300 ease-in-out overflow-hidden`}>
+                    <div className="flex flex-col h-full justify-center items-center">
                       {value.logo && (
-                        <div className="w-16 h-16 mb-4 sm:mr-4 sm:mb-0 flex-shrink-0">
+                        <div className="h-20 w-36 mb-4 sm:mr-4 sm:mb-0 ">
                           <img
                             src={value.logo}
                             alt={key}
-                            className="object-contain h-full w-full rounded-lg shadow-md"
+                            className="object-contain h-full w-full rounded-lg"
                           />
                         </div>
                       )}
-                      <div className="flex flex-col">
-                        <h3 className="text-lg font-bold mb-2 text-gray-900 text-center sm:text-left">{key}</h3>
-                        {value.description && <p className="text-sm mb-2 text-gray-700 text-center sm:text-left">{value.description}</p>}
+                      <div className="flex flex-col text-left">
+                        <h3 className="text-lg font-bold mb-2 text-gray-900">{key}</h3>
+                        {value.description && <p className="text-sm mb-2 text-gray-700">{value.description}</p>}
                         {value.url && (
-                          <p className="text-blue-600 hover:underline text-center sm:text-left">
-                            <a href={value.url} target="_blank" rel="noopener noreferrer">{value.url}</a>
+                          <p className="text-blue-600 hover:underline">
+                            <a href={value.url} target="_blank" rel="noopener noreferrer">Link to the community</a>
                           </p>
                         )}
                       </div>
