dcff07f65bb6a54fa504fb9d0452f00fc2075261
Kartik Patel <kartikpatel0170@gmail.com>
2023-06-30 12:06:28 -0600

    added landing page sections

---
+++ src/components/TestPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSection from "./HeroSection/HeroSection";
+import Sponsors from "./Sponsors/Sponsors";
+
+const TestPage = () => {
+  return (
+  <>
+  <HeroSection />
+  <Sponsors />
+  </>);
+};
+
+export default TestPage;
