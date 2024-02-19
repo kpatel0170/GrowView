f6403ffad7a3352003e5ba3c906539f606fbe161
kpatel0170 <kartikpatel0170@gmail.com>
2024-02-18 22:50:46 -0600

    Refactor header component and add logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,98 +1,76 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from 'react'
+import Container from "../Container"
+import Logo from "../Logo"
+import {Link} from "react-router-dom"
+import LogoutBtn from './LogoutBtn'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-export default function Header() {
-    return (
-        <header className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 bg-opacity-90 px-4 lg:px-6 py-2.5">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center">
-                        <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12 rounded-xl"
-                            alt="Logo"
-                        />
+
+function Header() {
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+
+    const navItems = [
+        {
+            name: "Home",
+            slug: "/",
+            active: true
+        },
+        {
+            name: "Login",
+            slug: "/login",
+            active: !authStatus
+        },
+        {
+            name: "Signup",
+            slug: "/signup",
+            active: !authStatus
+        },
+        {
+            name: "All Posts",
+            slug: "/all-posts",
+            active: authStatus
+        },
+        {
+            name: "Add Post",
+            slug: "/add-post",
+            active: authStatus
+        }
+    ]
+
+  return (
+    <header className='py-3 shadow bg-white bg-opacity-80 '>
+        <Container>
+            <nav className='flex'>
+                <div className='mr-4'>
+                    <Link to="/">
+                        <Logo width={120} />
                     </Link>
-                    <div className="flex items-center lg:order-2">
-                        <Link
-                            to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log in
-                        </Link>
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
-                        </Link>
-                    </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
-                    >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <NavLink
-                                to="/"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
-                                        ${isActive ? "text-orange-700" : "text-gray-700" } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/about"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
-                                        ${isActive ? "text-orange-700" : "text-gray-700" } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    About
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/contact"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
-                                        ${isActive ? "text-orange-700" : "text-gray-700" } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Contact
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/github"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
-                                        ${isActive ? "text-orange-700" : "text-gray-700" } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Github
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/user"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
-                                        ${isActive ? "text-orange-700" : "text-gray-700" } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    User
-                                </NavLink>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
                 </div>
+                <ul className='flex ml-auto'>
+                    {
+                        navItems.map((item) => item.active ? (
+                            <li key={item.name}>
+                                <button
+                                onClick={() => navigate(item.slug)}
+                                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                                >
+                                    {item.name}
+                                </button>
+                            </li>
+                        ) : null)
+                    }
+                    {authStatus && (
+                        <li>
+                            <LogoutBtn />
+                        </li>
+                    )}
+                </ul>
             </nav>
-        </header>
-    );
+        </Container>
+    </header>
+  )
 }
+
+export default Header
