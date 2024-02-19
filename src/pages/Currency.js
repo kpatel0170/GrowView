5eff75544b7731634fb5d15d07856c0a49109484
kpatel0170 <kartikpatel0170@gmail.com>
2024-02-18 22:52:36 -0600

    Add RTE component and AllPosts page

--- src/pages/Currency.js
+++ src/pages/Currency.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import useCurrencyInfo from '../hooks/useCurrencyInfo'
-import InputBox from '../components/InputBox'
+import InputBox from '../components/Input'
 
 const Currency = () => {
   const [amount, setAmount] = useState(0)
