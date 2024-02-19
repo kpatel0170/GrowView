ca6f61dc562c0ce1ea7d6f4e5b18277b5d7f9a87
kpatel0170 <kartikpatel0170@gmail.com>
2024-02-18 23:15:07 -0600

    Update imports and formatting

--- src/App.js
+++ src/App.js
@@ -1,58 +1,56 @@
 import React from "react";
- 
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Currency from "./pages/Currency";
 
-
-import { useState, useEffect } from 'react'
-import './App.css'
-import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { login, logout } from "./store/authSlice";
-import authService from './appwrite/auth'
+import authService from "./appwrite/auth";
 
-import { Provider } from 'react-redux'
-import store from "./store/store.js"
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
-
-import Home from "./pages/Home.jsx"
-import Login from "./pages/Login.jsx"
-import Protected from "./components/AuthLayout.jsx"
-import Signup from "./pages/Signup.jsx"
-import AllPosts from "./pages/AllPosts.jsx"
-import AddPost from "./pages/AddPost.jsx"
-import EditPost from "./pages/EditPost.jsx"
-import Post from "./pages/Post.jsx"
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Protected from "./components/AuthLayout.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
+import AddPost from "./pages/AddPost.jsx";
+import EditPost from "./pages/EditPost.jsx";
+import Post from "./pages/Post.jsx";
 
 function Layout() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
-    useEffect(() => {
-        authService
-            .getCurrentUser()
-            .then((userData) => {
-                if (userData) dispatch(login({ userData }));
-                else dispatch(logout());
-            })
-            .finally(() => setLoading(false));
-    }, [dispatch]);
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) dispatch(login({ userData }));
+        else dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
 
-    return !loading ? (
-      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
-          <div className="w-full block">
-              <Header />
-              <main>
-                  <Outlet />
-              </main>
-          </div>
-          <div className="w-full block">
-              <Footer />
-          </div>
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
       </div>
+      <div className="w-full block">
+        <Footer />
+      </div>
+    </div>
   ) : null;
 }
 
@@ -115,15 +113,15 @@ const router = createBrowserRouter([
       }
     ]
   }
-])
+]);
 const App = () => {
- return (
-   <>
-<Provider store={store}>
+  return (
+    <>
+      <Provider store={store}>
         <RouterProvider router={router} />
-    </Provider>
-   </>
- );
+      </Provider>
+    </>
+  );
 };
- 
+
 export default App;
